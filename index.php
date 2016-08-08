f67abd4b45c728a802c7f9ad66493b16beeaaff8
Drextd <5105490515@student.tafe>
2016-08-08 16:56:22 +1000

    First Commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="./css/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+<div id="container">
+
+    <header>
+        <div id="homeLogin">
+            <form id="home_login_style" name="homeLoginForm" method="post" action="">
+                <div>
+                    <label class="home_login_label" >Username/Email:</label><input class="home_login_input" type="text" name="home_username" placeholder="Username/Email">
+                </div>
+
+                <div>
+                    <label class="home_login_label" >Password:</label><input class="home_login_input" type="password" name="home_password" placeholder="Password">
+                </div>
+
+                <div>
+                    <div class="home_submit_style"></div>
+                    <input id="home_submit" type="submit" name="submit" value="Login">
+                    <div class="home_submit_style"></div>
+                </div>
+            </form>
+        </div>
+    </header>
+    
+    <nav>
+    </nav>
+
+    <section>
+    </section>
+
+    <footer>
+    </footer>
+
+</div>
+
+
+</body>
+</html>
